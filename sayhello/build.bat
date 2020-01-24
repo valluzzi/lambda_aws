a001543369b6eebf5f51b88b73f7842e25de10c7
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2020-01-24 11:11:03 +0100

    initial commit

---
+++ sayhello/build.bat
@@ -0,0 +1,7 @@
+@echo off
+rm function.zip
+cd package
+zip -r9 ..\function.zip .
+cd ..
+zip -g function.zip lambda_function.py
+aws lambda update-function-code --function-name sayhello --zip-file fileb://function.zip
---
+++ sayhello/lambda_function.py
@@ -0,0 +1,8 @@
+import json
+import numpy as np
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Valerio Again !')
+    }
